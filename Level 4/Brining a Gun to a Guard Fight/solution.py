7ddae98cc414244cc37f5b0640e30035280a99c1
deanzhou <dzhouwave@gmail.com>
2020-09-01 23:32:29 -0500

    starting level 4

---
+++ Level 4/Brining a Gun to a Guard Fight/solution.py
@@ -0,0 +1,5 @@
+def solution(dimensions, your_position, guard_position, distance):
+    #Your code here
+
+print(solution([3,2], [1,1], [2,1], 4))
+print(solution([300,275], [150,150], [185,100], 500))
