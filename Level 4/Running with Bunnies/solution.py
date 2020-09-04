e41d1fe77ea8dd728e06f57228329850eb5ba5a0
deanzhou <dzhouwave@gmail.com>
2020-09-04 01:13:51 -0500

    finished running with bunnies

--- Level 4/Running with Bunnies/solution.py
+++ Level 4/Running with Bunnies/solution.py
@@ -1,8 +1,26 @@
 result = []
 
 def dfs(currentID, totalTime, shortest_path, visited, times_limit):
+    global result
     if currentID == len(shortest_path) - 1:
-        if totalTime <= times_l
+        if totalTime <= times_limit: # legitimate path
+            thisResult = list(visited)
+            if 0 in thisResult:
+                thisResult.remove(0)
+            if len(shortest_path)-1 in thisResult:
+                thisResult.remove(len(shortest_path)-1)
+            if len(thisResult) > len(result):
+                result = thisResult
+            elif len(thisResult) == len(result) and thisResult < result:
+                result = thisResult
+
+    #generate new paths
+    for i in range(len(shortest_path)):
+        if i not in visited:
+            newVisited = list(visited)
+            newVisited.append(i)
+            dfs(i, totalTime + shortest_path[currentID][i], shortest_path, newVisited, times_limit)
+
 
 def solution(times, times_limit):
     global result
@@ -15,19 +33,22 @@ def solution(times, times_limit):
                 if shortest_path[i][j] > shortest_path[i][k] + shortest_path[k][j]:
                     shortest_path[i][j] = shortest_path[i][k] + shortest_path[k][j]
 
-    # print(shortest_path)
+    #print(shortest_path)
 
     #check for negative loop
     for i in range(len(shortest_path)):
         if shortest_path[i][i] < 0:
-            for j in range(1, len(shortest_path)-1)
+            for j in range(1, len(shortest_path)-1):
                 result.append(j-1)
             return result
 
     visited = []
     dfs(0, 0, shortest_path, list(visited), times_limit)
 
-
+    for i in range(len(result)):
+        result[i] -= 1
+    result.sort()
+    return result
 
 print(solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1))
 print(solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3))
